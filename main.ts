27a6d27b21247b2e6873e11775c2c4d6c158b866
Adrien Fauconnet <afauconnet@gmail.com>
2021-10-02 14:55:29 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onPinPressed(TouchPin.P0, function () {
+    radio.sendNumber(0)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    radio.sendNumber(2)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
